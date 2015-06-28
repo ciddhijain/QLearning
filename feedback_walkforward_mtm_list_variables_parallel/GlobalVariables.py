61fd246f099bfc0dd96ff19426a39cbb444e2be3
Ciddhi <ciddhijain@gmail.com>
2015-06-29 00:23:03 +0530

    Updating Global Variables

--- feedback_walkforward_mtm_list_variables_parallel/GlobalVariables.py
+++ feedback_walkforward_mtm_list_variables_parallel/GlobalVariables.py
@@ -2,7 +2,7 @@ __author__ = 'Ciddhi'
 
 from datetime import timedelta, datetime
 
-databaseName = 'QL_AXIS'                            # This is database name to which connection is made
+databaseName = 'QL_ALBK'                            # This is database name to which connection is made
 userName = 'root'                               # This is the user name for database connection
 password = 'controljp'                          # This is the password for database connection
 dbHost = '127.0.0.1'                            # This is host address for database connection
@@ -15,7 +15,7 @@ rankingDays = 15                                # This is the number of days for
 initializationDays = 15                         # This is the number of days for which q_matrix is initilaized
 liveDays = 30                                   # This is the number of days for which live trading is done
 
-logFileName = "QLearningAXIS"
+logFileName = "QLearningALBK"
 
 maxProcesses = 3                                # This is the number of maximum processes
 
@@ -23,8 +23,8 @@ maxProcesses = 3                                # This is the number of maximum 
 # These variables need to contain list values
 alpha = [0.5, 0.4, 0.6, 0.2, 0.8]                         # This defines the weightage to long trades as compared to short trades while constructing reward matrix
 gamma = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]                         # This defines the weightage of old data as compared to latest observations of reward matrix
-maxGreedyLevel = [5, 10]                 # This defines the number of future states for which reward is to be maximized in construction of q matrix
-individualFactor = [8, 12]               # This defines the factor of total asset which is to be allocated to each strategy
+maxGreedyLevel = [5]                 # This defines the number of future states for which reward is to be maximized in construction of q matrix
+individualFactor = [10]               # This defines the factor of total asset which is to be allocated to each strategy
 zeroRange = [0.002, 0.006, 0.01]                   # This determines the spread between states 0, 1, 2
 
 #------------------------------------------------------------------------------------------------------------------------------------------
@@ -38,8 +38,8 @@ trainingMaxTotalAsset = maxTotalAsset*trainingFactor        # This is the total 
 
 dummyPerformance = -50000
 
-performanceMonthlyOutfileNameBase = 'axis mtm monthly 15 15 30'
-performanceOutfileNameBase = 'axis mtm 15 15 30'
+performanceMonthlyOutfileNameBase = 'albk mtm monthly 15 15 30'
+performanceOutfileNameBase = 'albk mtm 15 15 30'
 
 latestIndividualTableBase = "latest_individual_table"
 trainingTradesheetTableBase = "training_tradesheet_data_table"
